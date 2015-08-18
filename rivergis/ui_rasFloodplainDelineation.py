b101857b630eac4df6e3675bc24bd89d29cf24e6
Radek Pasiok <radoslaw.pasiok@imgw.ad>
2015-08-18 09:24:08 +0200

    Usunięto zależność od DB Managera. Interfejs przerobiony na wzór HEC-GeoRAS.

--- rivergis/ui_rasFloodplainDelineation.py
+++ rivergis/ui_rasFloodplainDelineation.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_createDepthsAndFloodRange.ui'
+# Form implementation generated from reading ui file 'ui_rasFloodplainDelineation.ui'
 #
-# Created: Thu Mar 05 16:28:16 2015
+# Created: Thu Aug 13 13:30:27 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -23,28 +23,28 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_DlgCreateDepthsAndFloodRange(object):
-    def setupUi(self, DlgCreateDepthsAndFloodRange):
-        DlgCreateDepthsAndFloodRange.setObjectName(_fromUtf8("DlgCreateDepthsAndFloodRange"))
-        DlgCreateDepthsAndFloodRange.resize(294, 260)
-        self.gridLayout = QtGui.QGridLayout(DlgCreateDepthsAndFloodRange)
+class Ui_DlgFloodplainDelineation(object):
+    def setupUi(self, DlgFloodplainDelineation):
+        DlgFloodplainDelineation.setObjectName(_fromUtf8("DlgFloodplainDelineation"))
+        DlgFloodplainDelineation.resize(294, 260)
+        self.gridLayout = QtGui.QGridLayout(DlgFloodplainDelineation)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(DlgCreateDepthsAndFloodRange)
+        self.label = QtGui.QLabel(DlgFloodplainDelineation)
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout.addWidget(self.label)
-        self.cboDtm = QtGui.QComboBox(DlgCreateDepthsAndFloodRange)
+        self.cboDtm = QtGui.QComboBox(DlgFloodplainDelineation)
         self.cboDtm.setAcceptDrops(False)
         self.cboDtm.setObjectName(_fromUtf8("cboDtm"))
         self.verticalLayout.addWidget(self.cboDtm)
-        self.AreaNameAttributeLabel = QtGui.QLabel(DlgCreateDepthsAndFloodRange)
+        self.AreaNameAttributeLabel = QtGui.QLabel(DlgFloodplainDelineation)
         self.AreaNameAttributeLabel.setObjectName(_fromUtf8("AreaNameAttributeLabel"))
         self.verticalLayout.addWidget(self.AreaNameAttributeLabel)
-        self.cboWsel = QtGui.QComboBox(DlgCreateDepthsAndFloodRange)
+        self.cboWsel = QtGui.QComboBox(DlgFloodplainDelineation)
         self.cboWsel.setObjectName(_fromUtf8("cboWsel"))
         self.verticalLayout.addWidget(self.cboWsel)
-        self.tilesGroup = QtGui.QGroupBox(DlgCreateDepthsAndFloodRange)
+        self.tilesGroup = QtGui.QGroupBox(DlgFloodplainDelineation)
         self.tilesGroup.setObjectName(_fromUtf8("tilesGroup"))
         self.gridLayout_2 = QtGui.QGridLayout(self.tilesGroup)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
@@ -63,10 +63,10 @@ class Ui_DlgCreateDepthsAndFloodRange(object):
         self.verticalLayout.addWidget(self.tilesGroup)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.helpButton = QtGui.QPushButton(DlgCreateDepthsAndFloodRange)
+        self.helpButton = QtGui.QPushButton(DlgFloodplainDelineation)
         self.helpButton.setObjectName(_fromUtf8("helpButton"))
         self.horizontalLayout.addWidget(self.helpButton)
-        self.buttonBox = QtGui.QDialogButtonBox(DlgCreateDepthsAndFloodRange)
+        self.buttonBox = QtGui.QDialogButtonBox(DlgFloodplainDelineation)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.horizontalLayout.addWidget(self.buttonBox)
@@ -75,17 +75,17 @@ class Ui_DlgCreateDepthsAndFloodRange(object):
         self.label.setBuddy(self.cboDtm)
         self.AreaNameAttributeLabel.setBuddy(self.cboWsel)
 
-        self.retranslateUi(DlgCreateDepthsAndFloodRange)
-        QtCore.QMetaObject.connectSlotsByName(DlgCreateDepthsAndFloodRange)
+        self.retranslateUi(DlgFloodplainDelineation)
+        QtCore.QMetaObject.connectSlotsByName(DlgFloodplainDelineation)
 
-    def retranslateUi(self, DlgCreateDepthsAndFloodRange):
-        DlgCreateDepthsAndFloodRange.setWindowTitle(_translate("DlgCreateDepthsAndFloodRange", "Form", None))
-        self.label.setText(_translate("DlgCreateDepthsAndFloodRange", "DTM raster", None))
-        self.AreaNameAttributeLabel.setText(_translate("DlgCreateDepthsAndFloodRange", "Water surface elevation raster", None))
-        self.tilesGroup.setTitle(_translate("DlgCreateDepthsAndFloodRange", "Tiles", None))
-        self.AreaNameAttributeLabel_4.setText(_translate("DlgCreateDepthsAndFloodRange", "Tile buffer size", None))
-        self.tileSize.setText(_translate("DlgCreateDepthsAndFloodRange", "3000", None))
-        self.AreaNameAttributeLabel_3.setText(_translate("DlgCreateDepthsAndFloodRange", "Tile size", None))
-        self.bufferSize.setText(_translate("DlgCreateDepthsAndFloodRange", "50", None))
-        self.helpButton.setText(_translate("DlgCreateDepthsAndFloodRange", "Help", None))
+    def retranslateUi(self, DlgFloodplainDelineation):
+        DlgFloodplainDelineation.setWindowTitle(_translate("DlgFloodplainDelineation", "Floodplain Delineation", None))
+        self.label.setText(_translate("DlgFloodplainDelineation", "DTM raster", None))
+        self.AreaNameAttributeLabel.setText(_translate("DlgFloodplainDelineation", "Water surface elevation raster", None))
+        self.tilesGroup.setTitle(_translate("DlgFloodplainDelineation", "Tiles", None))
+        self.AreaNameAttributeLabel_4.setText(_translate("DlgFloodplainDelineation", "Tile buffer size", None))
+        self.tileSize.setText(_translate("DlgFloodplainDelineation", "3000", None))
+        self.AreaNameAttributeLabel_3.setText(_translate("DlgFloodplainDelineation", "Tile size", None))
+        self.bufferSize.setText(_translate("DlgFloodplainDelineation", "50", None))
+        self.helpButton.setText(_translate("DlgFloodplainDelineation", "Help", None))
 
