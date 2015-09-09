6c86494a4d0d1024626fc07ac4f0848cb19c3751
DamnBack <damnback333@gmail.com>
2015-09-09 15:26:58 +0200

    stations - cdn.

---
+++ PostgreSQL/StreamCenterline/from_to_stations.sql
@@ -0,0 +1,26 @@
+﻿CREATE OR REPLACE FUNCTION from_to_stations ()
+    RETURNS VOID AS
+$BODY$
+DECLARE
+    c cursor FOR SELECT * FROM "Endpoints"
+    r "Endpoints"%ROWTYPE;
+    rec revord;
+    river text;
+    fromsta double precision := 0;
+    tosta double precision :=0;
+BEGIN
+FOR r in c LOOP
+    river := r."RiverCode";
+    tosta := ST_Length(r.geom);
+    FOR i in 1... COUNT(SELECT * FROM "SreamCenterLine" WHERE "StreamCenterline"."ReachCode" = river) LOOP
+    
+    UPDATE "StreamCenterline" SET
+    "FromSta" = fromsta,
+    "ToSta" = tosta
+    WHERE CURRENT OF c;
+END LOOP;
+END;
+$BODY$
+    LANGUAGE plpgsql;
+
+SELECT from_to_stations ()
--- PostgreSQL/StreamCenterline/lengths_stations.sql
+++ PostgreSQL/StreamCenterline/lengths_stations.sql
@@ -2,25 +2,23 @@
 --DROP VIEW pnts2;
 --DROP VIEW pnts1;
 
-CREATE OR REPLACE VIEW pnts1 AS
+CREATE TABLE pnts1 AS
 SELECT "RiverCode", "ReachCode", ST_StartPoint(geom) AS geom, 'start' AS typ_punktu
 FROM "StreamCenterline"
 UNION ALL
 SELECT "RiverCode", "ReachCode", ST_EndPoint(geom) AS geom, 'end' AS typ_punktu
 FROM "StreamCenterline";
 
-CREATE OR REPLACE VIEW pnts2 AS
+CREATE TABLE pnts2 AS
 SELECT "RiverCode", geom
 FROM pnts1
 GROUP BY "RiverCode", geom
 HAVING COUNT(geom) = 1;
 
-DROP TABLE IF EXISTS endpoints;
-SELECT pnts1."RiverCode", pnts1."ReachCode", pnts1.geom::geometry(POINT, 2180) INTO endpoints
-FROM pnts1, pnts2
-WHERE pnts1."RiverCode" = pnts2."RiverCode" AND pnts1.geom = pnts2.geom AND pnts1.typ_punktu = 'end';
+DROP TABLE IF EXISTS "Endpoints";
+SELECT pnts1.geom::geometry(POINT, 2180), pnts1."RiverCode", pnts1."ReachCode", "NodesTable"."NodeID" INTO "Endpoints"
+FROM pnts1, pnts2, "NodesTable"
+WHERE pnts1."RiverCode" = pnts2."RiverCode" AND pnts1.geom = pnts2.geom AND pnts1.typ_punktu = 'end' AND pnts1.geom = "NodesTable".geom;
 
-DROP VIEW pnts1 CASCADE;
-
-SELECT * FROM "StreamCenterline"
-WHERE "StreamCenterline"."ReachCode" = ANY((SELECT endpoints."ReachCode" FROM endpoints))
+DROP TABLE pnts1;
+DROP TABLE pnts2;
