8bc738e4b655aa108076fdcdb152c5495a496f1a
Rafpar <pardarafal@gmail.com>
2015-09-16 14:23:54 +0200

    creating query for lateral structures and other small patches

--- PostgreSQL/Bridges_Culverts/bridges.sql
+++ PostgreSQL/Bridges_Culverts/bridges.sql
@@ -1,23 +1,23 @@
--- RiverCode and RiverReach update from StreamCenterline layer
+""" RiverCode and RiverReach update from StreamCenterline layer"""
 UPDATE mosty."Bridges" AS a
 SET "RiverCode" = b."RiverCode" , "ReachCode" = b."ReachCode"
 	FROM mosty."StreamCenterlines" AS b
     WHERE  a.geom && b.geom AND ST_Intersects(a.geom,b.geom);
 
--- generating of points needed for bridges Stationing calculation
+"""Generating of points needed for bridges Stationing calculation"""
 SELECT DISTINCT (ST_Dump(ST_Intersection(a.geom,b.geom))).geom AS geom,b."RiverCode", b."ReachCode", b."BridgeID"
 	INTO mosty.pkt
 		FROM mosty."StreamCenterlines" AS a, mosty."Bridges" as b
 		WHERE a.geom && b.geom;
 
--- calculation of bridges Stationing
+"""Calculation of bridges Stationing"""
 SELECT b."BridgeID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
 	INTO mosty.lokalizacja
 		FROM mosty."StreamCenterlines" AS a, mosty.pkt AS b
         WHERE a."ReachCode" = b."ReachCode"
         ORDER BY "ReachCode", "Station" ;
 
--- update of Bridges layer by Stationing values
+"""Update of Bridges layer by Stationing values"""
 UPDATE mosty."Bridges" AS a
 SET "Station" = b."Station"
 	FROM mosty.lokalizacja as b
--- PostgreSQL/Lateral_Structures/lateral_str.sql
+++ PostgreSQL/Lateral_Structures/lateral_str.sql
@@ -1,17 +1,11 @@
-CREATE TABLE mosty."Lateral_Structures" ("LateralStrID" serial primary key, "RiverCode" text, "ReachCode" text, "Station" double precision, "USDistance" double precision, "TopWidth" double precision, "NodeName" text, geom geometry);
-ALTER TABLE mosty."Lateral_Structures"
-    ALTER COLUMN geom TYPE geometry(LINESTRING,2180)
- 	USING ST_SetSRID(geom,2180);
-
-CREATE INDEX idx_bridges
-  	ON mosty."Lateral_Structures"
-  	 	USING gist(geom);
-
-
-SELECT ST_ClosestPoint(a.geom,ST_StartPoint(b.geom)) AS geom,a."RiverCode", a."ReachCode", b."LateralStrID"
+"""Looking closest point to the lateral structure on stream centerline layer and  
+calculating distance between lateral structure and stream centerline layer. 
+As a result we obtain number of points which intersects not only closest river line but all lines in naighborhood """
+DROP TABLE IF EXISTS mosty.pkt3;
+SELECT ST_ClosestPoint(a.geom,ST_StartPoint(b.geom)) AS geom, ST_Distance(a.geom,ST_StartPoint(b.geom)) AS dist, a."RiverCode", a."ReachCode", b."LateralStrID"
 	INTO mosty.pkt3
-		FROM mosty."StreamCenterlines" AS a, mosty."Lateral_Structures" as b
-		WHERE a.geom && b.geom;
+		FROM mosty."StreamCenterlines" AS a, mosty."Lateral_Structures" AS b
+		WHERE a.geom && b.geom ORDER BY b."LateralStrID";
 
 ALTER TABLE mosty.pkt3
     ALTER COLUMN geom TYPE geometry(POINT,2180)
@@ -21,18 +15,32 @@ CREATE INDEX idx_pkt3
   	ON mosty.pkt3
   	 	USING gist(geom);
 
+"""Selection of points which have the smalest distance to the river"""
+DROP TABLE IF EXISTS mosty.pkt4;
+SELECT "LateralStrID","RiverCode", "ReachCode", dist, geom INTO mosty.pkt4
+FROM (
+  SELECT "LateralStrID","RiverCode", "ReachCode", dist, geom, RANK() OVER (PARTITION BY "LateralStrID"  ORDER BY dist) AS rnk
+  FROM mosty.pkt3) AS sub
+WHERE rnk = 1;
 
+ALTER TABLE mosty.pkt4
+    ALTER COLUMN geom TYPE geometry(POINT,2180)
+ 	USING ST_SetSRID(geom,2180);
 
+CREATE INDEX idx_pkt4
+  	ON mosty.pkt4
+  	 	USING gist(geom);
 
-
--- SELECT b."LateralStrID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
--- 	INTO mosty.lokalizacja3
--- 		FROM mosty."StreamCenterlines" AS a, mosty.pkt3 AS b
---         WHERE a."ReachCode" = b."ReachCode"
---         ORDER BY "ReachCode", "Station" ;
---
--- UPDATE mosty."Lateral_Structures" AS a
--- SET "Station" = b."Station", "ReachCode" = b."ReachCode", "RiverCode" = b."RiverCode"
--- 	FROM mosty.lokalizacja3 as b
---     WHERE a."LateralStrID" = b."LateralStrID";
--- DROP TABLE mosty.pkt3, mosty.lokalizacja3;
+"""Calculation of Stationing for those points"""
+SELECT b."LateralStrID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
+	INTO mosty.lokalizacja3
+		FROM mosty."StreamCenterlines" AS a, mosty.pkt4 AS b
+        WHERE  a."ReachCode" = b."ReachCode"
+        ORDER BY "ReachCode", "Station" ;
+        
+"""Update Lateral_Structure layer by dane from previos step basing on LateralStrID """
+UPDATE mosty."Lateral_Structures" AS a
+SET "Station" = b."Station", "ReachCode" = b."ReachCode", "RiverCode" = b."RiverCode"
+	FROM mosty.lokalizacja3 as b
+    WHERE a."LateralStrID" = b."LateralStrID";
+DROP TABLE mosty.pkt3, mosty.pkt4, mosty.lokalizacja3;
