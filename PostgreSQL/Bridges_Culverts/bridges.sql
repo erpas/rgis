a1a0bfb4eb9f513f9e631bb95b373c8d9df01a9d
Rafpar <pardarafal@gmail.com>
2015-09-18 14:42:34 +0200

    small patches

--- PostgreSQL/Bridges_Culverts/bridges.sql
+++ PostgreSQL/Bridges_Culverts/bridges.sql
@@ -5,21 +5,23 @@ SET "RiverCode" = b."RiverCode" , "ReachCode" = b."ReachCode"
     WHERE  a.geom && b.geom AND ST_Intersects(a.geom,b.geom);
 
 -- Generating of points needed for bridges Stationing calculation
+DROP TABLE IF EXISTS "Dzierzgon".pkt;
 SELECT DISTINCT (ST_Dump(ST_Intersection(a.geom,b.geom))).geom AS geom,b."RiverCode", b."ReachCode", b."BridgeID"
-	INTO pkt
+	INTO "Dzierzgon".pkt
 		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon"."Bridges" as b
 		WHERE a.geom && b.geom;
 
 -- Calculation of bridges Stationing
+DROP TABLE IF EXISTS "Dzierzgon".lokalizacja;
 SELECT b."BridgeID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
-	INTO lokalizacja
-		FROM "Dzierzgon"."StreamCenterlines" AS a, pkt AS b
+	INTO "Dzierzgon".lokalizacja
+		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".pkt AS b
         WHERE a."ReachCode" = b."ReachCode"
         ORDER BY "ReachCode", "Station" ;
 
 -- Update of Bridges layer by Stationing values
 UPDATE "Dzierzgon"."Bridges" AS a
 SET "Station" = b."Station"
-	FROM lokalizacja as b
+	FROM "Dzierzgon".lokalizacja as b
     WHERE a."BridgeID" = b."BridgeID";
-DROP TABLE pkt, lokalizacja;
+DROP TABLE "Dzierzgon".pkt, "Dzierzgon".lokalizacja;
--- PostgreSQL/Inline_Structures/inline_str.sql
+++ PostgreSQL/Inline_Structures/inline_str.sql
@@ -5,21 +5,23 @@ SET "RiverCode" = b."RiverCode" , "ReachCode" = b."ReachCode"
     WHERE  a.geom && b.geom AND ST_Intersects(a.geom,b.geom);
 
 -- Generating of points needed for Inline_Structures Stationing calculation
+DROP TABLE IF EXISTS "Dzierzgon".pkt2;
 SELECT DISTINCT (ST_Dump(ST_Intersection(a.geom,b.geom))).geom AS geom,b."RiverCode", b."ReachCode", b."InlineStrID"
-	INTO pkt2
+	INTO "Dzierzgon".pkt2
 		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon"."Inline_Structures" as b
 		WHERE a.geom && b.geom;
 
 -- Calculation of Inline_Structures Stationing
+DROP TABLE IF EXISTS "Dzierzgon".lokalizacja2;
 SELECT b."InlineStrID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
-	INTO lokalizacja2
-		FROM "Dzierzgon"."StreamCenterlines" AS a, pkt2 AS b
+	INTO "Dzierzgon".lokalizacja2
+		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".pkt2 AS b
         WHERE a."ReachCode" = b."ReachCode"
         ORDER BY "ReachCode", "Station" ;
 
 -- Update of Inline_Structures layer by Stationing values
 UPDATE "Dzierzgon"."Inline_Structures" AS a
 SET "Station" = b."Station"
-	FROM lokalizacja2 as b
+	FROM "Dzierzgon".lokalizacja2 as b
     WHERE a."InlineStrID" = b."InlineStrID";
-DROP TABLE pkt2, lokalizacja2;
+DROP TABLE "Dzierzgon".pkt2, "Dzierzgon".lokalizacja2;
