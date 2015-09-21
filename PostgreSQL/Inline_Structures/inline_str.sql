ea8297096d97fd6140f2334a5dccb5236eef1d15
Rafpar <pardarafal@gmail.com>
2015-09-21 08:52:21 +0200

    syntax patches

--- PostgreSQL/Inline_Structures/inline_str.sql
+++ PostgreSQL/Inline_Structures/inline_str.sql
@@ -5,23 +5,23 @@ SET "RiverCode" = b."RiverCode" , "ReachCode" = b."ReachCode"
     WHERE  a.geom && b.geom AND ST_Intersects(a.geom,b.geom);
 
 -- Generating of points needed for Inline_Structures Stationing calculation
-DROP TABLE IF EXISTS "Dzierzgon".pkt2;
+DROP TABLE IF EXISTS "Dzierzgon".pointsinlinestr;
 SELECT DISTINCT (ST_Dump(ST_Intersection(a.geom,b.geom))).geom AS geom,b."RiverCode", b."ReachCode", b."InlineStrID"
-	INTO "Dzierzgon".pkt2
+	INTO "Dzierzgon".pointsinlinestr
 		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon"."Inline_Structures" as b
 		WHERE a.geom && b.geom;
 
 -- Calculation of Inline_Structures Stationing
-DROP TABLE IF EXISTS "Dzierzgon".lokalizacja2;
+DROP TABLE IF EXISTS "Dzierzgon".tempstatinlinestr;
 SELECT b."InlineStrID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
-	INTO "Dzierzgon".lokalizacja2
-		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".pkt2 AS b
+	INTO "Dzierzgon".tempstatinlinestr
+		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".pointsinlinestr AS b
         WHERE a."ReachCode" = b."ReachCode"
         ORDER BY "ReachCode", "Station" ;
 
 -- Update of Inline_Structures layer by Stationing values
 UPDATE "Dzierzgon"."Inline_Structures" AS a
 SET "Station" = b."Station"
-	FROM "Dzierzgon".lokalizacja2 as b
+	FROM "Dzierzgon".tempstatinlinestr as b
     WHERE a."InlineStrID" = b."InlineStrID";
-DROP TABLE "Dzierzgon".pkt2, "Dzierzgon".lokalizacja2;
+DROP TABLE "Dzierzgon".pointsinlinestr, "Dzierzgon".tempstatinlinestr;
--- PostgreSQL/Lateral_Structures/lateral_str.sql
+++ PostgreSQL/Lateral_Structures/lateral_str.sql
@@ -1,49 +1,49 @@
 
 -- Looking closest point to the lateral structure on stream centerline layer and calculating distance between
 -- lateral structure and stream centerline layer. As a result we obtain number of points which intersects
--- not only closest river line but all lines in naighborhood
+-- not only closest river line but all lines in neighborhood
 
-DROP TABLE IF EXISTS "Dzierzgon".pkt3;
+DROP TABLE IF EXISTS "Dzierzgon".riverclosestpoint1;
 SELECT ST_ClosestPoint(a.geom,ST_StartPoint(b.geom)) AS geom, ST_Distance(a.geom,ST_StartPoint(b.geom)) AS dist, a."RiverCode", a."ReachCode", b."LateralStrID"
-	INTO "Dzierzgon".pkt3
+	INTO "Dzierzgon".riverclosestpoint1
 		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon"."Lateral_Structures" AS b
 		WHERE a.geom && b.geom ORDER BY b."LateralStrID";
 
-ALTER TABLE "Dzierzgon".pkt3
+ALTER TABLE "Dzierzgon".riverclosestpoint1
     ALTER COLUMN geom TYPE geometry(POINT,2180)
  	USING ST_SetSRID(geom,2180);
 
-CREATE INDEX idx_pkt3
-  	ON "Dzierzgon".pkt3
+CREATE INDEX idx_riverclosestpoint1
+  	ON "Dzierzgon".riverclosestpoint1
   	 	USING gist(geom);
 
---- Selection of points which have the smalest distance to the river
-DROP TABLE IF EXISTS "Dzierzgon".pkt4;
-SELECT "LateralStrID","RiverCode", "ReachCode", dist, geom INTO "Dzierzgon".pkt4
+--- Selection of points which have the smallest distance to the river
+DROP TABLE IF EXISTS "Dzierzgon".riverclosestpoint2;
+SELECT "LateralStrID","RiverCode", "ReachCode", dist, geom INTO "Dzierzgon".riverclosestpoint2
 FROM (
   SELECT "LateralStrID","RiverCode", "ReachCode", dist, geom, RANK() OVER (PARTITION BY "LateralStrID"  ORDER BY dist) AS rnk
-  FROM "Dzierzgon".pkt3) AS temp
+  FROM "Dzierzgon".riverclosestpoint1) AS temp
 WHERE rnk = 1;
 
-ALTER TABLE "Dzierzgon".pkt4
+ALTER TABLE "Dzierzgon".riverclosestpoint2
     ALTER COLUMN geom TYPE geometry(POINT,2180)
  	USING ST_SetSRID(geom,2180);
 
-CREATE INDEX idx_pkt4
-  	ON "Dzierzgon".pkt4
+CREATE INDEX idx_riverclosestpoint2
+  	ON "Dzierzgon".riverclosestpoint2
   	 	USING gist(geom);
 
 --- Calculation of Stationing for those points
-DROP TABLE IF EXISTS "Dzierzgon".lokalizacja3;
+DROP TABLE IF EXISTS "Dzierzgon".tempstatclosestpoints;
 SELECT b."LateralStrID", b."RiverCode",b."ReachCode", (a."ToSta" - a."FromSta")*(1-ST_Line_Locate_Point(a.geom,b.geom)) AS "Station"
-	INTO "Dzierzgon".lokalizacja3
-		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".pkt4 AS b
+	INTO "Dzierzgon".tempstatclosestpoints
+		FROM "Dzierzgon"."StreamCenterlines" AS a, "Dzierzgon".riverclosestpoint2 AS b
         WHERE  a."ReachCode" = b."ReachCode"
         ORDER BY "ReachCode", "Station" ;
         
---- Update Lateral_Structure layer by dane from previos step basing on LateralStrID
+--- Update Lateral_Structure layer by dane from previous step basing on LateralStrID
 UPDATE "Dzierzgon"."Lateral_Structures" AS a
 SET "Station" = b."Station", "ReachCode" = b."ReachCode", "RiverCode" = b."RiverCode"
-	FROM "Dzierzgon".lokalizacja3 as b
+	FROM "Dzierzgon".tempstatclosestpoints as b
     WHERE a."LateralStrID" = b."LateralStrID";
-DROP TABLE "Dzierzgon".pkt3, "Dzierzgon".pkt4, "Dzierzgon".lokalizacja3;
+DROP TABLE "Dzierzgon".riverclosestpoint1, "Dzierzgon".riverclosestpoint2, "Dzierzgon".tempstatclosestpoints;
