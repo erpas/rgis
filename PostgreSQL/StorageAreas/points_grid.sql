7a1f2cfd7a727cb0ef7c9140d775db8138f43907
DamnBack <damnback333@gmail.com>
2015-10-09 16:18:33 +0200

    StorageAreas in PostGIS

--- PostgreSQL/StorageAreas/points_grid.sql
+++ PostgreSQL/StorageAreas/points_grid.sql
@@ -4,13 +4,16 @@ $BODY$
 DECLARE
     x double precision;
     y double precision;
+    pnt geometry;
+    inside boolean;
+    said integer;
     rows integer;
     cols integer;
-    step double precision := 10;
-    min_x double precision := (SELECT ST_Xmin(geom) FROM "Pasleka"."StorageAreas");
-    max_x double precision := (SELECT ST_Xmax(geom) FROM "Pasleka"."StorageAreas");
-    min_y double precision := (SELECT ST_Ymin(geom) FROM "Pasleka"."StorageAreas");
-    max_y double precision := (SELECT ST_Ymax(geom) FROM "Pasleka"."StorageAreas");
+    step double precision := 3;
+    min_x double precision := (SELECT ST_Xmin(ST_Extent(geom)) FROM "Pasleka"."StorageAreas");
+    max_x double precision := (SELECT ST_Xmax(ST_Extent(geom)) FROM "Pasleka"."StorageAreas");
+    min_y double precision := (SELECT ST_Ymin(ST_Extent(geom)) FROM "Pasleka"."StorageAreas");
+    max_y double precision := (SELECT ST_Ymax(ST_Extent(geom)) FROM "Pasleka"."StorageAreas");
 BEGIN
     rows := ceiling((max_x - min_x) / step);
     cols := ceiling((max_y - min_y) / step);
@@ -18,7 +21,12 @@ BEGIN
     FOR i IN 1..rows LOOP
         y := min_y;
         FOR j in 1..cols LOOP
-            INSERT INTO "Pasleka"."SASurface"(geom) VALUES (ST_SetSRID(ST_Point(x, y), 2180));
+            pnt := (ST_SetSRID(ST_Point(x, y), 2180));
+	    SELECT ST_Within(pnt, geom), "StorageID" INTO inside, said FROM "Pasleka"."StorageAreas" WHERE ST_Within(pnt, geom) IS True;
+            IF inside is True THEN
+		INSERT INTO "Pasleka"."SASurface"(geom, "StorageID")
+		VALUES (pnt, said);
+	    END IF;
             y := y + step;
         END LOOP;
         x := x + step;
@@ -27,6 +35,7 @@ END;
 $BODY$
     LANGUAGE plpgsql;
 
+
 DROP TABLE IF EXISTS "Pasleka"."SASurface";
-CREATE TABLE "Pasleka"."SASurface"(geom geometry(POINT, 2180), id bigserial primary key);
+CREATE TABLE "Pasleka"."SASurface"(geom geometry(POINT, 2180), id bigserial primary key, "StorageID" integer, "Elevation" double precision);
 SELECT "Pasleka".points_grid ();
---
+++ PostgreSQL/StorageAreas/storage_calculator.sql
@@ -0,0 +1,23 @@
+﻿CREATE OR REPLACE FUNCTION "Pasleka".storage_calculator ()
+    RETURNS VOID AS
+$BODY$
+DECLARE
+    lev double precision := 0;
+    area double precision := 9;
+    emax double precision := (SELECT MAX("Elevation") FROM "Pasleka".sa_points);
+    emin double precision := (SELECT MIN("Elevation") FROM "Pasleka".sa_points);
+    division integer := 5;
+    h double precision := (emax - emin) / division;
+BEGIN
+    FOR i IN 1..division LOOP
+        INSERT INTO "Pasleka".levels (start_level, end_level, volume)
+        SELECT lev, lev + h, SUM("Elevation")*h FROM "Pasleka".sa_points WHERE "Elevation" BETWEEN lev AND lev + h;
+        lev := lev + h;
+    END LOOP;
+END;
+$BODY$
+    LANGUAGE plpgsql;
+
+DROP TABLE IF EXISTS "Pasleka".levels;
+CREATE TABLE "Pasleka".levels(start_level double precision, end_level double precision, volume double precision);
+SELECT "Pasleka".storage_calculator ();
