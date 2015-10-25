3d3c014e0765750e573afd6ea32cff25a7742f5f
rpasiok <rpasiok@gmail.com>
2015-10-25 22:16:47 +0100

    new RiverGIS icon

--- rivergis/ui/ui_rivergis.py
+++ rivergis/ui/ui_rivergis.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/ui_rivergis.ui'
 #
-# Created: Fri Oct 23 15:41:39 2015
+# Created: Fri Oct 23 20:01:20 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
