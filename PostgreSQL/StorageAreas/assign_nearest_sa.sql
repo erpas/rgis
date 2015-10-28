0486eecf53180ee58a05639173a82c0200c3735e
DamnBack <damnback333@gmail.com>
2015-10-28 15:25:07 +0100

    Added SAConnections methods.

---
+++ PostgreSQL/StorageAreas/assign_nearest_sa.sql
@@ -0,0 +1,12 @@
+﻿WITH distances AS
+    (SELECT
+        "StorageID",
+        ST_Distance(sa.geom, ST_StartPoint(sac.geom)) AS usdist,
+        ST_Distance(sa.geom, ST_EndPoint(sac.geom)) AS dsdist
+    FROM
+        "Pasleka"."StorageAreas" AS sa,
+        "Pasleka"."SAConnections" AS sac)
+UPDATE "Pasleka"."SAConnections"
+SET
+    "USSA" = (SELECT "StorageID" FROM distances ORDER BY usdist LIMIT 1),
+    "DSSA" = (SELECT "StorageID" FROM distances ORDER BY dsdist LIMIT 1)
